a3bd10331ffdff47130b45728b85cae284bedec8
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-09-21 18:01:43 +0100

    Position update, unfinished

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import logo from './images/LogoWhite.png';
 import './App.css';
 import SocialWrapper from './components/SocialWrapper'
 
-// uninstall FontAwesome
-import { FaRegFileAlt, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaRegFileAlt, FaGithub, FaLinkedin, FaMapPin } from 'react-icons/fa';
 import { FiMail } from 'react-icons/fi'
 import { IconContext } from "react-icons"
 
@@ -14,11 +13,17 @@ function App() {
 			<img src={logo} className="My-logo slide-in-bck-center slide-in-elliptic-bottom-bck" alt="mylogo" />
 		</div>
 		<h1 className="slide-in-elliptic-bottom-bck">Javier Sánchez Fernández</h1>
-		<p className="slide-in-elliptic-bottom-bck">I'm a last-year Computer Science student based in Madrid, Spain.</p>
+		<p className="slide-in-elliptic-bottom-bck">I'm a new grad working as a Software Engineer at <a className="a-link" href="https://canonical.com/">Canonical</a> (the makers of Ubuntu)</p>
 		<p className="slide-in-elliptic-bottom-bck">
-		<IconContext.Provider value={{ color: "white", size: "1.5em", className: "global-class-name" }}>
-			<FiMail/> sanchezfdezjavier@gmail.com
-		</IconContext.Provider>
+		<div className="email-location-container">
+			<IconContext.Provider value={{ color: "white", size: "1.5em", className: "global-class-name" }}>
+				<FiMail/> sanchezfdezjavier@gmail.com
+			</IconContext.Provider>
+			
+			<IconContext.Provider value={{ color: "white", size: "1.5em", className: "global-class-name" }}>
+				<FaMapPin/> Dublin, Ireland
+			</IconContext.Provider>
+		</div>
 		</p>
 		<i className="fas fa-camera"></i>
 		<div className="row slide-in-elliptic-bottom-bck">
