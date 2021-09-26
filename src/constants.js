74b28b9dc7d9fbc031a6f258b13ff93bccf1cbb9
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-09-26 11:03:05 +0100

    URL, and h1 font title changed

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const resumeURL = "https://docs.google.com/document/d/1yjZM0yORhAmvz0hcGOjrZ4MK1KgP68buQTprcvOTkzM/edit"
+export const resumeURL = "https://docs.google.com/document/d/1yjZM0yORhAmvz0hcGOjrZ4MK1KgP68buQTprcvOTkzM/edit?usp=sharing"
 export const linkedinURL = "https://www.linkedin.com/in/sanchezfdezjavier"
 export const githubURL = "https://github.com/sanchezfdezjavier"
