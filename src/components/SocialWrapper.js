a6ce7bb554731946e8d8f283c46089059e43033a
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-10-09 17:10:31 +0100

    Feat: Added hover effect to social network buttons

--- src/components/SocialWrapper.js
+++ src/components/SocialWrapper.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { IconContext } from "react-icons";
+import "./SocialWrapper.css";
 
 const socialWrapper = (props) => {
   return (
     <IconContext.Provider
       value={{ color: "white", size: "2em", className: "global-class-name" }}
     >
-      <div className="button">
+      <div className="button" className="chipp">
         <a href={props.link} target="_blank" rel="noopener noreferrer">
           {props.children}
         </a>
